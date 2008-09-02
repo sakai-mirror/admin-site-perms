c2a5f3861d732f284820cc9fb63a4f5574467869
aaronz@vt.edu <aaronz@vt.edu@fdecad78-55fc-0310-b1b2-d7d25cf747c9>
2008-09-02 15:44:04 +0000

    Updated to make the authzgroup changes a little more efficient hopefully
    git-svn-id: https://source.sakaiproject.org/contrib/caret/admin-site-perms/trunk@52663 fdecad78-55fc-0310-b1b2-d7d25cf747c9

--- tool/src/java/org/sakaiproject/adminsiteperms/tool/ActionBean.java
+++ tool/src/java/org/sakaiproject/adminsiteperms/tool/ActionBean.java
@@ -15,7 +15,10 @@
 package org.sakaiproject.adminsiteperms.tool;
 
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -41,139 +44,159 @@ import uk.org.ponder.messageutil.TargettedMessageList;
  */
 public class ActionBean {
 
-   private static Log log = LogFactory.getLog(ActionBean.class);
+    private static Log log = LogFactory.getLog(ActionBean.class);
 
-   private SiteService siteService;
-   public void setSiteService(SiteService siteService) {
-      this.siteService = siteService;
-   }
+    private SiteService siteService;
+    public void setSiteService(SiteService siteService) {
+        this.siteService = siteService;
+    }
 
-   private AuthzGroupService authzGroupService;
-   public void setAuthzGroupService(AuthzGroupService authzGroupService) {
-      this.authzGroupService = authzGroupService;
-   }
+    private AuthzGroupService authzGroupService;
+    public void setAuthzGroupService(AuthzGroupService authzGroupService) {
+        this.authzGroupService = authzGroupService;
+    }
 
-   private SecurityService securityService;
-   public void setSecurityService(SecurityService securityService) {
-      this.securityService = securityService;
-   }
+    private SecurityService securityService;
+    public void setSecurityService(SecurityService securityService) {
+        this.securityService = securityService;
+    }
 
-   private TargettedMessageList messages;
-   public void setMessages(TargettedMessageList messages) {
-      this.messages = messages;
-   }
+    private TargettedMessageList messages;
+    public void setMessages(TargettedMessageList messages) {
+        this.messages = messages;
+    }
 
-   /**
-    * The types of sites to add perms to (course/project/workspace/etc.)
-    */
-   public String[] types;
-   /**
-    * The perms to add/remove to the sites
-    */
-   public String[] perms;
-   /**
-    * The roles to have the perms added or removed
-    */
-   public String[] roles;
+    /**
+     * The types of sites to add perms to (course/project/workspace/etc.)
+     */
+    public String[] types;
+    /**
+     * The perms to add/remove to the sites
+     */
+    public String[] perms;
+    /**
+     * The roles to have the perms added or removed
+     */
+    public String[] roles;
 
-   @SuppressWarnings("unchecked")
-   public String addSiteRolePerms() {
-      try {
-         checkInputs();
-      } catch (IllegalArgumentException e) {
-         messages.addMessage( new TargettedMessage("user.message.illegal.submission",
-               new Object[] { }, TargettedMessage.SEVERITY_ERROR));
-         return "failed";
-      }
-      // now add the perms to all matching roles in all matching sites
-      setSiteRolePerms(true);
-      return "success";
-   }
+    public String addSiteRolePerms() {
+        try {
+            checkInputs();
+        } catch (IllegalArgumentException e) {
+            messages.addMessage( new TargettedMessage("user.message.illegal.submission",
+                    new Object[] { }, TargettedMessage.SEVERITY_ERROR));
+            return "failed";
+        }
+        // now add the perms to all matching roles in all matching sites
+        setSiteRolePerms(true);
+        return "success";
+    }
 
-   public String removeSiteRolePerms() {
-      try {
-         checkInputs();
-      } catch (IllegalArgumentException e) {
-         messages.addMessage( new TargettedMessage("user.message.illegal.submission",
-               new Object[] { }, TargettedMessage.SEVERITY_ERROR));
-         return "failed";
-      }
-      // now remove the perms from all matching roles in all matching sites
-      setSiteRolePerms(false);
-      return "success";
-   }
+    public String removeSiteRolePerms() {
+        try {
+            checkInputs();
+        } catch (IllegalArgumentException e) {
+            messages.addMessage( new TargettedMessage("user.message.illegal.submission",
+                    new Object[] { }, TargettedMessage.SEVERITY_ERROR));
+            return "failed";
+        }
+        // now remove the perms from all matching roles in all matching sites
+        setSiteRolePerms(false);
+        return "success";
+    }
 
-   @SuppressWarnings("unchecked")
-   protected void setSiteRolePerms(boolean add) {
-      String permsString = makeStringFromArray(perms);
-      String typesString = makeStringFromArray(types);
-      String rolesString = makeStringFromArray(roles);
-      List<String> permsList = Arrays.asList(perms);
-      // now add the perms to all matching roles in all matching sites
-      List<Site> sites = siteService.getSites(SelectionType.ANY, types, null, null, SortType.NONE, null);
-      int successCount = 0;
-      for (Site site : sites) {
-         String siteRef = site.getReference();
-         try {
-            AuthzGroup ag = authzGroupService.getAuthzGroup(siteRef);
-            if (authzGroupService.allowUpdate(ag.getId())) {
-               for (String role : roles) {
-                  Role r = ag.getRole(role);
-                  // if role not found in this group then move on
-                  if (r != null) {
-                     if (add) {
-                        r.allowFunctions(permsList);
-                     } else {
-                        r.disallowFunctions(permsList);
-                     }
-                     authzGroupService.save(ag);
-                     log.info("Added Permissions ("+permsString+") for role ("+role+") to group:" + siteRef);
-                  }
-               }
-               successCount++;
-            } else {
-               log.warn("Cannot update authz group: " + siteRef + ", unable to apply any perms change");
+    @SuppressWarnings("unchecked")
+    protected void setSiteRolePerms(boolean add) {
+        String permsString = makeStringFromArray(perms);
+        String typesString = makeStringFromArray(types);
+        String rolesString = makeStringFromArray(roles);
+        List<String> permsList = Arrays.asList(perms);
+        // now add the perms to all matching roles in all matching sites
+        List<Site> sites = siteService.getSites(SelectionType.ANY, types, null, null, SortType.NONE, null);
+        int successCount = 0;
+        for (Site site : sites) {
+            String siteRef = site.getReference();
+            try {
+                AuthzGroup ag = authzGroupService.getAuthzGroup(siteRef);
+                if (authzGroupService.allowUpdate(ag.getId())) {
+                    boolean updated = false;
+                    for (String role : roles) {
+                        Role r = ag.getRole(role);
+                        // if role not found in this group then move on
+                        if (r != null) {
+                            // get the current perms so we can possibly avoid an update
+                            Set<String> current = r.getAllowedFunctions();
+                            if (add) {
+                                if (! current.containsAll(permsList)) {
+                                    // only update if the perms are not already there
+                                    r.allowFunctions(permsList);
+                                    updated = true;
+                                }
+                            } else {
+                                boolean found = false;
+                                for (String perm : permsList) {
+                                    if (current.contains(perm)) {
+                                        found = true;
+                                        break;
+                                    }
+                                }
+                                if (found) {
+                                    // only update if at least one perm needs to be removed
+                                    r.disallowFunctions(permsList);
+                                    updated = true;
+                                }
+                            }
+                        }
+                    }
+                    if (updated) {
+                        // only save if the group was updated
+                        authzGroupService.save(ag);
+                        log.info("Added Permissions ("+permsString+") for roles ("+rolesString+") to group:" + siteRef);
+                    }
+                    successCount++;
+                } else {
+                    log.warn("Cannot update authz group: " + siteRef + ", unable to apply any perms change");
+                }
+            } catch (GroupNotDefinedException e) {
+                log.error("Could not find authz group: " + siteRef + ", unable to apply any perms change");
+            } catch (AuthzPermissionException e) {
+                log.error("Could not save authz group: " + siteRef + ", unable to apply any perms change");
             }
-         } catch (GroupNotDefinedException e) {
-            log.error("Could not find authz group: " + siteRef + ", unable to apply any perms change");
-         } catch (AuthzPermissionException e) {
-            log.error("Could not save authz group: " + siteRef + ", unable to apply any perms change");
-         }
-      }
-      int failureCount = sites.size() - successCount;
-      String operation = (add ? "adding" : "removing");
-      messages.addMessage( new TargettedMessage("user.message.permissions." + (add ? "added" : "removed"),
-            new Object[] { typesString, rolesString, permsString, sites.size(), successCount, failureCount }, 
-            TargettedMessage.SEVERITY_INFO));
-      log.info("Completed "+operation+" permissions ("+permsString+") in "+sites.size()
-            + " sites of types ("+typesString+") for the following roles ("+rolesString+"), "
-            + "there were "+successCount+" successful updates and "+failureCount+" failures");
-   }
+        }
+        int failureCount = sites.size() - successCount;
+        String operation = (add ? "adding" : "removing");
+        messages.addMessage( new TargettedMessage("user.message.permissions." + (add ? "added" : "removed"),
+                new Object[] { typesString, rolesString, permsString, sites.size(), successCount, failureCount }, 
+                TargettedMessage.SEVERITY_INFO));
+        log.info("Completed "+operation+" permissions ("+permsString+") in "+sites.size()
+                + " sites of types ("+typesString+") for the following roles ("+rolesString+"), "
+                + "there were "+successCount+" successful updates and "+failureCount+" failures");
+    }
 
-   protected String makeStringFromArray(String[] array) {
-      StringBuilder sb = new StringBuilder();
-      for (int i = 0; i < array.length; i++) {
-         if (i > 0) {
-            sb.append(", ");
-         }
-         sb.append(array[i]);
-      }
-      return sb.toString();
-   }
+    protected String makeStringFromArray(String[] array) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            if (i > 0) {
+                sb.append(", ");
+            }
+            sb.append(array[i]);
+        }
+        return sb.toString();
+    }
 
-   protected void checkInputs() {
-      if (types == null || types.length == 0) {
-         throw new IllegalArgumentException("site types cannot be null or empty");
-      }
-      if (perms == null || perms.length == 0) {
-         throw new IllegalArgumentException("perms to add/remove cannot be null or empty");
-      }
-      if (roles == null || roles.length == 0) {
-         throw new IllegalArgumentException("roles to add/remove perms for cannot be null or empty");
-      }
-      // TODO validate the perms list?
-      if (! securityService.isSuperUser()) {
-         throw new SecurityException("Only the super user can do mass updates of permissions");
-      }
-   }
+    protected void checkInputs() {
+        if (types == null || types.length == 0) {
+            throw new IllegalArgumentException("site types cannot be null or empty");
+        }
+        if (perms == null || perms.length == 0) {
+            throw new IllegalArgumentException("perms to add/remove cannot be null or empty");
+        }
+        if (roles == null || roles.length == 0) {
+            throw new IllegalArgumentException("roles to add/remove perms for cannot be null or empty");
+        }
+        // TODO validate the perms list?
+        if (! securityService.isSuperUser()) {
+            throw new SecurityException("Only the super user can do mass updates of permissions");
+        }
+    }
 }
